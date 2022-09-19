f49d0cbd61370ec302b888c66ef5ed24418d1fd2
sh970901 <aaddss639@naver.com>
2022-09-20 01:03:48 +0900

    test용 디비

--- src/test/java/com/ll/exam/dockerexam/DockerExamApplicationTests.java
+++ src/test/java/com/ll/exam/dockerexam/DockerExamApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.exam.dockerexam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DockerExamApplicationTests {
 
 	@Test
@@ -11,3 +13,4 @@ class DockerExamApplicationTests {
 	}
 
 }
+
